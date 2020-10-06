258e2a441643f6a7bf819e88c2c266c65f443599
Enmanuel Gonzalez <liriano23_@hotmail.com>
2020-10-06 17:57:50 -0400

    Adding Partion Operator section

---
+++ Conversion-Operators-In-LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conversion_Operators_In_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Partition-Operator-In-Linq/Program.cs
+++ Partition-Operator-In-Linq/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Partition_Operator_In_Linq
 {
@@ -6,7 +8,42 @@ namespace Partition_Operator_In_Linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string[] countries = {"India", "USA", "Russia", "China", "Australia","Argentina", "United Kingdom"};
+
+            //IEnumerable<string> result = countries.Take(3);
+
+            //LINQ Take Operator in Query Syntax Example
+            //IEnumerable<string> result = (from x in countries select x).Take(3);
+
+            /*
+                Take While -> This one make a partition chosing the elements than 
+                comply with the conditions 
+            */
+
+            string[] countries = { "US", "UK", "Russian", "China", "Australia", "Argentina" };
+            //IEnumerable<string> result = countries.TakeWhile(x =>  x.StartsWith("U"));
+
+            //Example of LINQ TakeWhile in Query Syntax
+            //IEnumerable<string> result = (from x in countries select x).TakeWhile(x => x.StartsWith("U"));
+            
+            /*
+                //Skip -> Avoid the number of elements in the paramater and 
+                return the remaining ones
+            */
+
+           //IEnumerable<string> result = countries.Skip(3);
+
+           //Query Sintax
+           //IEnumerable<string> result = (from x in countries select x).Skip(3);
+
+            //Skip while 
+            IEnumerable<string> result = countries.SkipWhile(x => x.StartsWith("U"));
+            foreach(var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+
         }
     }
 }
