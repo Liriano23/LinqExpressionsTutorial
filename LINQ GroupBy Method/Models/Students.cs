595ea3965775bf6b07771f3ef38eb5494ed34f9e
Enmanuel Gonzalez <liriano23_@hotmail.com>
2020-10-15 17:58:24 -0400

    Adding Group by Section

---
+++ LINQ GroupBy Method/Models/Students.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace LINQ_GroupBy_Method.Models
+{
+    public class Students
+    {
+        public string Name{get; set;}
+        public string Gender{get; set;}
+        public string Location{get; set;}
+
+    }
+    
+}
---
+++ LINQ GroupBy Method/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using LINQ_GroupBy_Method.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_GroupBy_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Students> objStudent = new List<Students>()
+            {
+                new Students() { Name = "Suresh Dasari", Gender = "Male",Location="Chennai" },
+                new Students() { Name = "Rohini Alavala", Gender = "Female", Location="Chennai" },
+                new Students() { Name = "Praveen Alavala", Gender = "Male",Location="Bangalore" },
+                new Students() { Name = "Sateesh Alavala", Gender = "Male", Location ="Vizag"},
+                new Students() { Name = "Madhav Sai", Gender = "Male", Location="Nagpur"}
+            };
+
+            var Student = objStudent.GroupBy(x=> x.Location);
+
+            foreach (var item in Student)
+            {
+                Console.WriteLine(item.Key+"  "+ item.Count());
+                Console.WriteLine();
+
+                foreach(var item2 in item)
+                {
+                    Console.WriteLine(item2.Name + "\t" + item2.Location);
+                    Console.WriteLine();
+                }
+
+
+            }
+        }
+    }
+}
